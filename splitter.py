b9ebf6b97f0e4e837fdc2fdf18874a198c149994
jamcinnamon <ellie0334@gmail.com>
2025-02-23 15:46:51 +0900

    ADD: .gitignore and code modify

--- splitter.py
+++ splitter.py
@@ -20,15 +20,29 @@ loader = CSVLoader(
     file_path="crawled_results_cleaned.csv",
     content_columns=["result"],  # 검색할 내용이 있는 컬럼
     metadata_columns=["coverletter"],  # 메타데이터로 사용할 컬럼
+    source_column='coverletter',
     encoding="utf-8"
 )
 
 
 documents = loader.load()
-
+# print(documents)
 
 embedding = OpenAIEmbeddings()
 
 vector_store = InMemoryVectorStore.from_documents(documents, embedding)
 
-print(f'총 DB 안의 다큐 갯수: {len(documents)}')
+# print(f'총 DB 안의 다큐 갯수: {len(documents)}')
+
+vector_store.add_documents(documents=documents)
+
+query = '''
+Excellent communication and negotiation skills, .
+Ability to work independently and collaboratively
+'''
+
+results = vector_store.similarity_search_with_score(
+    query=query, k=1
+)
+for doc, score in results:
+    print(f"* [SIM={score:3f}] [{doc.metadata}]")
